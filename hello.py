d7f1d34d6d98fcd0f5ebee71c1893b1b9a6d5757
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2018-08-02 23:48:23 +0500

    Form submition with GET and POST methods

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,30 @@ def about():
 @app.route('/user/<username>')
 def user(username):
     return 'Hello Dear: '+username
+
+
+@app.route("/form")
+def form():
+    return ''' 
+<form action="/info" method="POST">
+<input type="text" name="user" placeholder="Full Name"><br>
+<input type="email" name="email" placeholder="Email"><br>
+<input type="password" name="pass" placeholder="password"><br>
+<input type="submit" value="Send">
+</form>
+'''
+
+@app.route("/info", methods=['GET','POST'])
+def info():
+    if request.method == "GET":
+        return "<h1>GET Method</h1> Hello "+ request.args.get('user') + \
+    " <br> Your Email: "+request.args.get('email') + \
+    " <br> Your password: "+request.args.get('pass')
+    else:
+        return "<h1>POST Method</h1>Hello "+ request.form['user'] + \
+    " <br> Your Email: "+request.form['email'] + \
+    " <br> Your password: " + request.form['pass']
+
 
 app.run(debug='true')
 
