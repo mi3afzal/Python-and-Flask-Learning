f37fdc2730e81c6e5317b88f1ba13baece27c86c
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2018-08-12 12:33:18 +0500

    implement search and inset to mongo DB.

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME']='bootcamp'
+#app.config['MONGO_URI']='mongodb://qasim:qasim@ds143030.mlab.com:43030/miti'
+app.config['MONGO_URI']='mongodb://irfan:pass123@ds219532.mlab.com:19532/bootcamp'
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -39,6 +45,30 @@ def info():
         return "<h1>POST Method</h1>Hello "+ request.form['user'] + \
     " <br> Your Email: "+request.form['email'] + \
     " <br> Your password: " + request.form['pass']
+
+
+@app.route("/users")
+def users():
+    students=[
+        {'id':1, 'name':'Qasim'},
+        {'id':2, 'name': 'Zeeshan hanif', 'education':'PHD'}
+    ]
+    return jsonify({'SaylaniStudents': students})
+
+
+@app.route("/mongodb")
+def mongodb():
+    students = []
+    records = mongo.db.users.find({'name': 'Muhammad Irfan Afzal'})
+    for user in records:
+        students.append({'name': user['name']})
+    return jsonify({'SaylaniStudents': students})
+
+
+@app.route("/add_user_to_mongodb")
+def add_user_to_mongodb():
+    add = mongo.db.users.insert({'name': 'kashan'})
+    return "Successfully add"
 
 
 app.run(debug='true')
